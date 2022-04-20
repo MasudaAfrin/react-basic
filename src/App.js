59f1f2ebab7bf326238dfc83fc22ff02f8526a49
MasudaAfrin <masudatuba@gmail.com>
2022-04-20 11:52:02 +0600

    classcomponent with state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import {Intro} from './components/Intro';
 import ClassComponent from './components/ClassComponent';
 import Jsx from './components/Jsx';
 import FunProps from './components/FunProps';
-import ClassProps from './components/ClassProps'
+import ClassProps from './components/ClassProps';
+import ClassState from './components/ClassHook'
+import ClassHook from './components/ClassHook';
 
 function App() {
   return (
@@ -15,7 +17,8 @@ function App() {
       <ClassComponent/>
       <Jsx /> */}
       {/* <FunProps name='Tuba'/> */}
-      <ClassProps name='Tuba'/>
+      {/* <ClassProps name='Tuba'/> */}
+      <ClassHook/>
     </div>
   );
 }
---
+++ src/components/ClassHook.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class ClassHook extends Component {
+    constructor(){
+        super();
+        this.state = {
+            message: 'Welcome Visitor',
+            status: 0,
+            btn_txt: 'Subscribe'
+        } 
+    }
+
+    changeMessage(){
+        if(this.state.status === 0){
+            this.setState({
+                message: 'Thank you for subscribing',
+                status: 1,
+                btn_txt: 'Unsubscribe'
+            })
+        } else{
+            this.setState({
+                message: 'Welcome Visitor',
+                status: 0,
+                btn_txt: 'Subscribe'
+            })
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>
+                   {this.state.message}
+                </h1>
+                <button onClick={() => this.changeMessage()}>{this.state.btn_txt}</button>
+            </div>
+        )
+    }
+};
+
+export default ClassHook;
